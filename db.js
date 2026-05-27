cc360e0deff24c9d5a70c60e9962c3764a81c466
hexdanher <hexdanher_dev@outlook.com>
2026-05-26 22:58:49 -0500

    fix: add DB_PORT to mysql connection

--- db.js
+++ db.js
@@ -7,12 +7,12 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'docentesuniversidad'
-
+    database: process.env.DB_NAME || 'docentesuniversidad',
+    port: Number(process.env.DB_PORT) || 3306
 });
 
 connection.connect((err) => {
-    if (err){
+    if (err) {
         console.log('Error connecting to database', err);
         return;
     }
